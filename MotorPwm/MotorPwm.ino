c635c595c1b9e6bcf946825bd548d1435f8c1e74
Zevensuy Rodriguez <zevenwolf@gmail.com>
2012-03-06 12:23:55 -0500

    pwm

--- MotorPwm/MotorPwm.ino
+++ MotorPwm/MotorPwm.ino
@@ -1,9 +1,18 @@
-//MOTORSHIELD BASICS
-//PWM-Set Speed
-//BRAKE- Start and Stop Motor
-//DIRECTION - Clockwise and Counter-clock
-
-// give the motor control pins names:
+  /*
+  Motor Shield Setup
+  
+  Motor 1
+  Direction: Digital Pin 12
+  PWM: Digital Pin 3
+  Brake: Digital Pin 9
+  Current Sense: Analog Pin A0
+  
+  Motor 1
+  Direction: Digital Pin 13
+  PWM: Digital Pin 11
+  Brake: Digital Pin 8
+  Current Sense: Analog Pin A1  
+  */
 const int pwmA = 3;
 const int brakeA = 9;
 const int dirA = 12;
@@ -11,7 +20,7 @@ const int pwmB = 11;
 const int brakeB = 8;
 const int dirB = 13;
 
-int x = 0;
+
 void setup() {
   Serial.begin(9600);
   pinMode(pwmA, OUTPUT);
@@ -22,46 +31,21 @@ void setup() {
   pinMode(dirB, OUTPUT);
 
 }
+
 void loop() {
-  //Move motorA counter-clockwise
-  x = 0b101000;
-  setMotorPins(x);
 
-  delay(1000);
-
-  x = 0b111000;
-  setMotorPins(x);
-  delay(1000);
-
-  //Move motorA clockwise
-  x = 0b100000;
-  setMotorPins(x);
-
-  delay(1000);
-
-  x = 0b110000;
-  setMotorPins(x);
-  delay(1000);
-
+  int sensorValue = analogRead(A5)/4;
+  Serial.println(sensorValue);
+  
+  analogWrite(pwmA, sensorValue);
+  digitalWrite(brakeA, LOW);
+  digitalWrite(dirA, HIGH); 
+  /* Motor 2
+  int sensorValue = analogRead(A5)/4;
+  Serial.println(sensorValue);
+  
+  analogWrite(pwmA, sensorValue);
+  digitalWrite(brakeA, LOW);
+  digitalWrite(dirA, HIGH); 
+  */
 }
-
-void setMotorPins(byte pinState) {
-  // pring the pin states as a binary number:
-  // Serial.println(pinState, BIN); 
-
-  // read each of the 6 bits and assign each to a pin:
-  digitalWrite(pwmA, bitRead(pinState, 5));
-  digitalWrite(brakeA, bitRead(pinState, 4));
-  digitalWrite(dirA, bitRead(pinState, 3));  
-  digitalWrite(pwmB, bitRead(pinState, 2));
-  digitalWrite(brakeB, bitRead(pinState, 1));
-  digitalWrite(dirB, bitRead(pinState, 0));
-
-}
-
-
-
-
-
-
-
